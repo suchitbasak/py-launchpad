50951b649c0d0ce2eddb065ebeb16d7c36503fd9
suchi <suchitbasak@gmail.com>
2024-08-30 00:46:09 +0100

    Made the main script more modular, easier to read. Still need to make the light transitions on the launchpad smoother

--- main.py
+++ main.py
@@ -2,71 +2,41 @@
 1. Read audio
 2. Perform FFT
 3. Change light on the Novation Launchpad
-
-Optional:
-Have a little gui with the launchpad in it
 '''
 
 import time
-import numpy as np
-import pyaudio
-from launchpad import light_up_matrix, everything_off
+import keyboard
+from functions_launchpad import light_up_launchpad, everything_off
+from functions_audio import init_steam, start_recording, stop_recording, get_fft, fft_to_8x8matrix
 
-GRID_ROWS = 8
-GRID_HEIGHT = 8
-
-# Part 1: Record audio
 
 # Using the default mic because I am basic b.
 
-CHUNKSIZE = 10000 # fixed chunk size
+everything_off()
 
 # initialize portaudio
-p = pyaudio.PyAudio()
+p = init_steam()
 
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=1000, input=True, frames_per_buffer=CHUNKSIZE)
-
+print("Press 'q' to quit")
 
 while True:
-    print("Recording in progress...")
-    try:
-        chunky_bytes = stream.read(CHUNKSIZE, exception_on_overflow = False)
-    except:
-        pass
+    # start recording data
+    stream_object, audio_data = start_recording(p)
+
+    fft_audio_data = get_fft(audio_data)
+    print(fft_audio_data)
+
+    # convert fft to nice matrix that can be displayed on launchpad
+    matrix_for_launchpad = fft_to_8x8matrix(fft_audio_data)
+
+    light_up_launchpad(position_matrix=matrix_for_launchpad, color=120)
+
+    if keyboard.is_pressed('q'):
+        print('Closing stream')
+        stop_recording(stream_object=stream_object, p=p)
+        everything_off()
+        break
     
-    chunky_np = np.frombuffer(chunky_bytes, dtype=np.int16)
-
-    # close stream
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    print("Recording over")
-
-    # FFFFFFFFT
-    chunky_freq = np.fft.fft(chunky_np, GRID_ROWS)
-
-    # normalize and quantize this
-    chunky_norm = np.abs(chunky_freq)/ max(np.abs(chunky_freq))
-    chunky_norm = GRID_HEIGHT * chunky_norm
-    chunky_round = np.round(chunky_norm)
-
     
-    # Convert to 8x8 matrix to give input to function for launchpad
-    mat = np.zeros((8,8))
-    for i in range(8):
-        for j in range(int(chunky_round[i])):
-            mat[i,j] = 1
-
-
-    mat =np.flip(mat,1)
-    mat = np.transpose(mat)
-    mat = np.flip(mat,0)
-
-    light_up_matrix(position_matrix=mat, color=12)
-    time.sleep(0.1)
+    time.sleep(0.5))
     everything_off()
-
-# plot data
-#plt.plot(chunky_np)
-#plt.show()
